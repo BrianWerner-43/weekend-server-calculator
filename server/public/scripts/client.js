c390bbd9f8e5fe105b4c864b20887baf568b1ee0
Brian Werner <brianmw199@gmail.com>
2023-11-04 16:13:37 -0500

    Set up the functionality for my operator buttons and sourced in css

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,101 @@
 console.log('client.js is sourced!');
+
+function formSubmit(event) {
+    event.preventDefault();
+
+};
+
+function clearBtn(){
+    document.getElementById('first-num').value = '';
+    document.getElementById('second-num').value = '';
+    // When first number is enetered we want the input to claer
+    // after the equals or clear button is clicked
+};
+
+// Creating a function for the addBtn so when clicked it will capture the user wants to use the add operator
+ let operator = '';
+function addBtn() {
+    operator = '+';
+  console.log(operator);
+  return operator;
+ 
+ 
+};
+
+// This function will subtract
+function subtractBtn() {
+    operator = '-';
+  console.log(operator);
+  return operator;
+ 
+ 
+};
+
+//This function will multiply
+function multiplyBtn() {
+    operator = '*';
+  console.log(operator);
+  return operator;
+ 
+ 
+};
+
+// This function will divide
+function divideBtn() {
+    operator = '/';
+  console.log(operator);
+  return operator;
+ 
+ 
+};
+
+//  equal button Capture numbers from the inputs to send to the server
+   // within the equal button should cature the value from the inputs
+   // bundle up the inputs in an object
+   // then send the object to the server via Post route
+
+// function equalsBtn() {
+//     let firstNumber = document.getElementById('first-num').value;
+//     let secondNumber = document.getElementById('second-num').value;
+//     document.getElementById('first-num').value = '';
+//     document.getElementById('second-num').value = '';
+
+//      let equals = {
+//             numOne: Number(firstNumber),
+//             numTwo: Number(secondNumber),
+//             operator: operator
+            
+            
+//         }
+    
+//     console.log('Testing equalsBtn', equals);
+
+//     axios({
+//         method: 'POST',
+//         url: '/calculations',
+//         data: equals
+//     }).then((response) => {
+//         getResults();
+//     })
+
+// };
+
+// // Setting up a function getResults to get results from the server via GET route
+// function getResults() {
+//     axios({
+//         method: 'GET',
+//         url: '/calculations'
+//     }).then((response) =>{
+        
+//         console.log('Testing the GET');
+//     })
+
+// };
+
+
+
+
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-let PORT = process.env.PORT || 5000;
+let PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(express.static('server/public'));
@@ -10,13 +10,43 @@ app.use(express.static('server/public'));
 let calculations = []
 
 
+
 // Here's a wonderful place to make some routes:
 
-// GET /calculations
+// GET /calculations 
+// app.get('/calculations', (req, res) => {
+//   console.log('GET/ calculations is getting request');
+//   console.log('GET/ calculations req.body', req.body);
+//   console.log('Expect results', calculations);
+//   res.send(calculations);
+// })
 
-// POST /calculations
+// // POST /calculations
+// app.post('/calculations', (req, res) => {
+//    doMath(req.body)
+//   console.log('POST /calculations is getting request');
+//   console.log('POST /calculations req.body:', req.body);
+//   res.sendStatus(201)
+// });
 
+// // Function for doing math and pushing into calaculations array
+// function doMath(object) {
+//   if(object.operator === '+') {
+//      object.result = object.numOne + object.numTwo
+//      calculations.push(object);
+//   } else if(object.operator === '-') {
+//     object.result = object.numOne - object.numTwo
+//     calculations.push(object);
+//   } else if(object.operator === '*') {
+//     object.result = object.numOne * object.numTwo
+//     calculations.push(object);
+//   } else if(object.operator === '/') {
+//     object.result = object.numOne / object.numTwo
+//     calculations.push(object);
+//   }
+//   console.log(object);
 
+// };
 // PLEASE DO NOT MODIFY ANY CODE BELOW THESE BEARS:
 // 🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸
 
